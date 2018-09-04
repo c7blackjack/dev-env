01eb0efc6004b714941adff59786e50f185ae68d
TravisLloyd <jackson231@Traviss-iMac.local>
2018-09-04 02:26:07 -0700

    Added Testing and Continuous Integration

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,7 @@ import open from'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 
 //Constant declarations for use below
 const port = 3000;
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,7 @@
 import chalk from 'chalk';
 
+/* eslint-disable no-console */
+
 
 //This is a freindly message used as a pre hook using script automation
 console.log(chalk.green('Starting app in dev mode...'));
