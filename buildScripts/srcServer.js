b3de8d1d618199246890a0434aa6a3a469fc7d45
Travis Lloyd <travy@Ambers-MacBook-Pro.local>
2018-09-04 23:53:02 -0700

    Added fetch functionality

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -22,6 +22,17 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+    //Replace hardcode with actual database
+    res.json([
+        {"id": 1,"firstName":"Travis","lastName":"Barker","email":"travisbarker@misc.com"},
+        {"id": 2,"firstName":"John","lastName":"Smith","email":"johnsmith@misc.com"},
+        {"id": 3,"firstName":"Steve","lastName":"Nash","email":"stevenash@misc.com"},
+        {"id": 4,"firstName":"Barbara","lastName":"Sorento","email":"barbarasorento@misc.com"}
+
+    ]);
+});
+
 //Starts listening on port for req
 app.listen(port, function(err) {
     if(err) {
