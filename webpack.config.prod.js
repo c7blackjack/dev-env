c6e536278799cccff7c216e59120fc4ef49edd26
TravisLloyd <jackson231@Traviss-iMac.local>
2018-09-08 08:11:50 -0700

    configured production webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,34 @@
 import path from 'path';
+import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { truncate } from 'fs';
 
 export default {
   debug: true,
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   noInfo: false,
   entry: [
     path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
   plugins: [
-     //This creates a HTML file that references bundle.js
-     new HtmlWebpackPlugin({
+    //This creates a HTML file that references bundle.js
+    new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject = true
-     })
-  ],
+    }),
 
+    //Filter out any duplicate packages
+    new webpack.optimize.DedupePlugin(),
+
+    //Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
