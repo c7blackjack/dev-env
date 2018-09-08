5794b2b031a9b5b100f8188721df33c73ac57aae
TravisLloyd <jackson231@Traviss-iMac.local>
2018-09-08 08:11:06 -0700

    Removed calls to webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,21 +1,11 @@
 import express from 'express';
 import path from 'path';
 import open from'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev';
-
 /* eslint-disable no-console */
 
 //Constant declarations for use below
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
-
-//Adding webpack to app
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}));
 
 //Sends initial start up page upon request
 app.get('/', function(req, res) {
